e1e1d4897e3d0e555b19964f831220215d630bbe
Jasonsantoso <santoso.jason13@gmail.com>
2017-04-07 23:01:10 -0700

    styling fixed

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /* Get locations pages */
 module.exports.homelist = function(req, res) {
-	res.render('locations-list', { title: 'Home' });
+	res.render('locations-list', {
+		title: 'Loc8r - find a place to work with wifi',
+		pageHeader: {
+			title: 'Loc8r',
+			strapline: 'Find a place to work with wifi near you!'
+		},
+		locations: [{
+			name: 'Starcups',
+			rating: 3,
+			distance: '100m',
+			address: '125 High Street, Reading, RG6 1PS',
+			facilities: ['Hot drinks', 'Foods', 'Premium wifi'] 
+		},{
+			name: 'Del taco',
+			rating: 2,
+			distance: '150m',
+			address: '125 High Street, Reading, RG6 1PS',
+			facilities: ['Hot drinks', 'Foods'] 
+		},{
+			name: "Denny's",
+			rating: 3,
+			distance: '100m',
+			address: '125 High Street, Reading, RG6 1PS',
+			facilities: ['Hot drinks', 'Foods', 'Premium wifi'] 
+		}] 
+	});
 };
 
 module.exports.locationInfo = function(req, res) {
@@ -8,5 +33,5 @@ module.exports.locationInfo = function(req, res) {
 };
 
 module.exports.addReview = function(req, res) {
-	res.render('index', { title: 'Add review' });
+	res.render('location-review-form', { title: 'Add review' });
 };
