a4266409150c1aaccdbcf1126278fbc90a9f4c29
Jason <stovewebdev@gmail.com>
2017-10-04 23:03:46 -0700

    start chapter 7

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,12 @@
-/* Get locations pages */
-module.exports.homelist = function(req, res) {
+var request = require("request");
+var apiOptions = {
+	server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+	apiOptions.server = "https://jasonsantoso.herokuapp.com";
+}
+
+var renderHomepage = function(req, res, responseBody) {
 	res.render('locations-list', {
 		title: 'Loc8r - find a place to work with wifi',
 		pageHeader: {
@@ -7,26 +14,30 @@ module.exports.homelist = function(req, res) {
 			strapline: 'Find a place to work with wifi near you!'
 		},
 		sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let loc8r help you find the place you're looking for.",
-		locations: [{
-			name: 'Starcups',
-			rating: 3,
-			distance: '100m',
-			address: '125 High Street, Reading, RG6 1PS',
-			facilities: ['Hot drinks', 'Foods', 'Premium wifi'] 
-		},{
-			name: 'Del Taco',
-			rating: 2,
-			distance: '150m',
-			address: '125 High Street, Reading, RG6 1PS',
-			facilities: ['Hot drinks', 'Foods'] 
-		},{
-			name: "Denny's",
-			rating: 4,
-			distance: '200m',
-			address: '125 High Street, Reading, RG6 1PS',
-			facilities: ['Hot drinks', 'Foods', 'Premium wifi'] 
-		}] 
+		locations: responseBody
 	});
+}
+
+/* Get locations pages */
+module.exports.homelist = function(req, res) {
+	var requestOptions, path;
+	path = '/api/locations';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method : "GET",
+		json : {},
+		qs : {
+			lng : -0.7992599,
+			lat : 51.378091,
+			maxDistance : 20
+		}
+	};
+	request(
+		requestOptions, 
+		function(err, response, body) {
+			renderHomepage(req, res, body);
+		}
+	);
 };
 
 module.exports.locationInfo = function(req, res) {
